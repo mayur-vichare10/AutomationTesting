13c253fa3148dadf9eb5d695d53dbbf707f6bcf8
mayur-vichare10 <mayur_vichare@ymail.com>
2021-07-14 19:12:08 +0530

    Uploading Data to Github

---
+++ src/test/java/AutomationTestCases/IgniteSolutions/AppTest.java
@@ -0,0 +1,20 @@
+package AutomationTestCases.IgniteSolutions;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
